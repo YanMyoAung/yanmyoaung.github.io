fbbf513345e5f5ad8ac22af36c61a5b31ff2e3cb
YanMyoAung <pandora.2068@gmail.com>
2023-06-29 16:54:39 +0630

    update site

---
+++ resources/js/index.js
@@ -0,0 +1,7 @@
+function menuOnClick() {
+    document.getElementById("menu-bar").classList.toggle("change");
+    document.getElementById("menu-bar").classList.toggle("menu-bar-margin");
+    document.getElementById("nav").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("nav-margin");
+    document.getElementById("menu-bg").classList.toggle("change-bg");
+}
