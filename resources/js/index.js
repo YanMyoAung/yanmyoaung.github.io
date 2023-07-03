2ca90064326ca007c7bd3e67e044888157c9aa7d
YanMyoAung <pandora.2068@gmail.com>
2023-07-03 22:15:03 +0630

    add_circle_nav_in_header

--- resources/js/index.js
+++ resources/js/index.js
@@ -5,3 +5,19 @@ function menuOnClick() {
     document.getElementById("nav").classList.toggle("nav-margin");
     document.getElementById("menu-bg").classList.toggle("change-bg");
 }
+
+const circle_txt = "S T A R T  Y O U R   J O U R N E Y";
+const circle_block = document.getElementById("circle_text");
+window.onload = () => {
+    for (let i = 0; i < circle_txt.length; i++) {
+        let span = document.createElement("span");
+        span.innerHTML = circle_txt[i];
+        span.classList.add('circle_text');
+        circle_block.appendChild(span);
+        span.style.transform = `rotate(${-100 + (10 * i)}deg)`;
+        if (i === 0 || i === 11 || i === 21) {
+            span.style.color = '#e60576';
+            span.style.fontSize = '11px';
+        }
+    }
+}
